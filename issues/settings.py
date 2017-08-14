0bb4854ce37641459a69d9e9c18bcb30acc1654b
Can Mustafa Ozdemir <canmustafaozdemir@gmail.com>
2017-08-14 15:40:06 +0300

    dev and base settings and requirements

--- issues/settings.py
+++ issues/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'debug_toolbar',
-    'django_extensions',
-
     'issue_tracker',
 ]
 
@@ -52,10 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
-INTERNAL_IPS = ['127.0.0.1']
 
 ROOT_URLCONF = 'issues.urls'
 
